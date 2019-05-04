0a558d181cc546597b109b729af198c1d53d36cf
Tom Auger <tomauger@live.com>
2019-05-04 19:19:06 +0100

    Add public encrypt method

--- secretbox/SecretBox.cs
+++ secretbox/SecretBox.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Diagnostics.Contracts;
+    using System.Security.Cryptography;
     using System.Text;
     using static Internal.Primitive;
     using static Utilities;
@@ -20,46 +21,18 @@
         private const byte TagHeader = 0x01;
         private const byte TagPayload = 0x02;
 
-        private static void Setup(byte[] buf, long msgId, string ctx, byte[] key, byte[] iv, byte keyTag)
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+
+        public static void Encrypt(
+            byte[] c, byte[] msg, int mlen, long msgId, string ctx, byte[] key)
         {
-            Contract.Assert(buf.Length == GimliBlockBytes);
-            Contract.Assert(ctx.Length == ContextBytes);
+            var iv = new byte[IVBytes];
+            rngCsp.GetBytes(iv);
 
-            // Zero out the buffer
-            Array.Clear(buf, 0, GimliBlockBytes);
-
-            // Add the prefix and context and apply the Gimli permutation
-            Array.Copy(Prefix, buf, Prefix.Length);
-            // TODO: Change the interface of this method to have ctx a byte array and 
-            // do the conversion in the calling function
-            var ctxBytes = Encoding.UTF8.GetBytes(ctx);
-            Array.Copy(ctxBytes, 0, buf, Prefix.Length, ContextBytes);
-            Contract.Assert(Prefix.Length + ContextBytes == GimliRate);
-            Gimli(buf, TagHeader);
-
-            // Add the key and apply the Gimli permutation
-            Contract.Assert(KeyBytes == 2 * GimliRate);
-            ArrayXor(key, 0, buf, 0, GimliRate);
-            Gimli(buf, keyTag);
-            ArrayXor(key, GimliRate, buf, 0, GimliRate);
-            Gimli(buf, keyTag);
-
-            // Add the IV and apply the Gimli permutation
-            Contract.Assert(IVBytes < GimliRate * 2);
-            buf[0] ^= IVBytes;
-            ArrayXor(iv, 0, buf, 1, GimliRate - 1);
-            Gimli(buf, TagHeader);
-            ArrayXor(iv, GimliRate - 1, buf, 0, IVBytes - (GimliRate - 1));
-
-            // Add the msgId and apply the Gimli permutation.
-            // Convert the int64 msgId to an array of 8 bytes
-            Contract.Assert(IVBytes - GimliRate + 8 <= GimliRate);
-            var msgIdBytes = BitConverter.GetBytes(msgId);
-            ArrayXor(msgIdBytes, 0, buf, IVBytes - GimliRate, 8);
-            Gimli(buf, TagHeader);
+            EncryptWithIv(c, msg, mlen, msgId, ctx, key, iv);
         }
 
-        private static void EncryptIv(
+        private static void EncryptWithIv(
             byte[] c, byte[] msg, int mlen, long msgId, string ctx, byte[] key, byte[] iv)
         {
             var buf = new byte[GimliBlockBytes];
@@ -105,13 +78,43 @@
             ArrayCopy(buf, GimliRate, mac, 0, MACBytes);
         }
 
-        private static void Finalize(byte[] buf, byte[] key, byte tag)
+        private static void Setup(byte[] buf, long msgId, string ctx, byte[] key, byte[] iv, byte keyTag)
         {
-            Contract.Assert(KeyBytes == GimliCapacity);
-            ArrayXor(key, 0, buf, GimliRate, KeyBytes);
-            Gimli(buf, tag);
-            ArrayXor(key, 0, buf, GimliRate, KeyBytes);
-            Gimli(buf, tag);
+            Contract.Assert(buf.Length == GimliBlockBytes);
+            Contract.Assert(ctx.Length == ContextBytes);
+
+            // Zero out the buffer
+            Array.Clear(buf, 0, GimliBlockBytes);
+
+            // Add the prefix and context and apply the Gimli permutation
+            Array.Copy(Prefix, buf, Prefix.Length);
+            // TODO: Change the interface of this method to have ctx a byte array and 
+            // do the conversion in the calling function
+            var ctxBytes = Encoding.UTF8.GetBytes(ctx);
+            Array.Copy(ctxBytes, 0, buf, Prefix.Length, ContextBytes);
+            Contract.Assert(Prefix.Length + ContextBytes == GimliRate);
+            Gimli(buf, TagHeader);
+
+            // Add the key and apply the Gimli permutation
+            Contract.Assert(KeyBytes == 2 * GimliRate);
+            ArrayXor(key, 0, buf, 0, GimliRate);
+            Gimli(buf, keyTag);
+            ArrayXor(key, GimliRate, buf, 0, GimliRate);
+            Gimli(buf, keyTag);
+
+            // Add the IV and apply the Gimli permutation
+            Contract.Assert(IVBytes < GimliRate * 2);
+            buf[0] ^= IVBytes;
+            ArrayXor(iv, 0, buf, 1, GimliRate - 1);
+            Gimli(buf, TagHeader);
+            ArrayXor(iv, GimliRate - 1, buf, 0, IVBytes - (GimliRate - 1));
+
+            // Add the msgId and apply the Gimli permutation.
+            // Convert the int64 msgId to an array of 8 bytes
+            Contract.Assert(IVBytes - GimliRate + 8 <= GimliRate);
+            var msgIdBytes = BitConverter.GetBytes(msgId);
+            ArrayXor(msgIdBytes, 0, buf, IVBytes - GimliRate, 8);
+            Gimli(buf, TagHeader);
         }
 
         private static void XorEnc(byte[] buf, ArraySegment<byte> output, ArraySegment<byte> input, int inputLength)
@@ -133,6 +136,15 @@
 
             Pad(buf, leftOver, GimliDomainAEAD);
             Gimli(buf, TagPayload);
+        }
+
+        private static void Finalize(byte[] buf, byte[] key, byte tag)
+        {
+            Contract.Assert(KeyBytes == GimliCapacity);
+            ArrayXor(key, 0, buf, GimliRate, KeyBytes);
+            Gimli(buf, tag);
+            ArrayXor(key, 0, buf, GimliRate, KeyBytes);
+            Gimli(buf, tag);
         }
 
         private static void Pad(byte[] buf, int pos, byte domain)
