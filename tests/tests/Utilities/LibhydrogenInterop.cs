e0f66bde261e25b959041105018a561d28dfe152
Tom Auger <tomauger@live.com>
2019-05-06 09:56:29 +0100

    Add libhydrogen interop to tests

---
+++ tests/tests/Utilities/LibhydrogenInterop.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests
+{
+    using System.Runtime.InteropServices;
+
+    internal static class LibhydrogenInterop
+    {
+        private const string LibhydrogenDll = "Libhydrogen.dll";
+
+        [DllImport(LibhydrogenDll, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int hydro_init();
+
+        [DllImport(LibhydrogenDll, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int hydro_secretbox_encrypt(
+            byte[] c,
+            byte[] m,
+            int mlen,
+            long msg_id,
+            string ctx,
+            byte[] key);
+
+        [DllImport(LibhydrogenDll, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int hydro_secretbox_decrypt(
+            byte[] m,
+            byte[] c,
+            int clen,
+            long msg_id,
+            string ctx,
+            byte[] key);
+    }
+}
